56b55e3c063cc5ecee128c59d68ebf561004bcb1
sergey-msu <spmathf@gmail.com>
2024-08-22 12:45:38 +0300

    swap tokens from exact

---
+++ scripts/UniswapV2/swapTokensForExactTokens.js
@@ -0,0 +1,56 @@
+const { ethers } = require('hardhat')
+
+const {
+    printEvents,
+    useContract,
+    getContract,
+    loadContractAddress,
+    sendSimpleMessage,
+}  = require('../utils.js');
+const { printBalances } = require('./utils.js');
+
+
+async function main() {
+    const crossChainLayerContract = await useContract('ICrossChainLayer', process.env.EVM_CCL_ADDRESS);
+    const appProxyContract = await getContract('UniswapV2Proxy', 'UniswapV2Proxy');
+
+    await printBalances('\nBalances before operation');
+
+    const tokenA = loadContractAddress('TKA');
+    const tokenB = loadContractAddress('TKB');
+    const amountInMax = 10n * 10n**9n;
+    const amountOut = 13n * 10n**9n;
+    const path = [tokenA, tokenB];
+    const to = await appProxyContract.getAddress();
+    const deadline = 19010987500n;
+    const message = {
+        target: await appProxyContract.getAddress(),
+        methodName: 'swapTokensForExactTokens(uint256,uint256,address[],address,uint256)',
+        arguments: new ethers.AbiCoder().encode(
+            ['uint256', 'uint256', 'address[]', 'address', 'uint256'],
+            [
+                amountOut, 
+                amountInMax, 
+                path, 
+                to, 
+                deadline,
+            ]
+        ),
+        caller: 'EQB4EHxrOyEfeImrndKemPRLHDLpSkuHUP9BmKn59TGly2Jk',
+        mint: [
+            {tokenAddress: tokenA, amount: amountInMax},
+        ],
+        unlock: [],
+    };
+
+    const tx = await sendSimpleMessage(message);
+    const receipt = await tx.wait()
+
+    await printBalances('\nBalances after operation');
+
+    console.log('\n------------------- Events -------------------\n')
+    printEvents(receipt, crossChainLayerContract);
+};
+
+
+main();
