2db2f2ae9de1a9452802336de14469aaee00f203
GavrilenkoDS <dg@bemo.finance>
2024-08-25 18:16:25 +0300

    added curve templates

---
+++ scripts/CurveLite/addLiquidity.js
@@ -0,0 +1,62 @@
+const { ethers } = require('hardhat')
+
+const {
+    useContract,
+    getContract,
+    printEvents,
+    loadContractAddress,
+    sendSimpleMessage,
+}  = require('../utils.js');
+const { printBalances } = require('./utils.js');
+
+
+async function main(showEvents=false) {
+    const crossChainLayerContract = await useContract('ICrossChainLayer', process.env.EVM_CCL_ADDRESS);
+    const appProxyContract = await getContract('CurveLiteProxy', 'CurveLiteProxy', null, process.env.UNISWAPV2_PROXY_ADDRESS);
+    
+    //await printBalances('\nBalances before operation');
+
+    const tokenA = loadContractAddress('TKA');
+    const tokenB = loadContractAddress('TKB');
+    const pool = '';
+    const pool_type = 20 ;// pool_type: 10 - stable-ng, 20 - twocrypto-ng, 30 - tricrypto-ng, 4 - llamma
+    const swap_params = [[0,1,4,20],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+    const to = await appProxyContract.getAddress();
+    const amount = 0;
+    const min_dy = 0
+
+    const deadline = 19010987500n;
+
+    const NA = 0x0000000000000000000000000000000000000000
+
+    const message = {
+        target: await appProxyContract.getAddress(),
+        methodName: 'exchange(address[11], uint256[4][5], uint256, uint256)',
+        arguments: new ethers.AbiCoder().encode(
+            ['address[11]', 'uint256[4][5]', 'uint256', 'uint256', 'address'],
+            [
+                [tokenA,pool,tokenB,NA,NA,NA,NA,NA,NA,NA,NA], 
+                swap_params, 
+                amount, 
+                min_dy,
+            ]
+        ),
+        caller: 'EQB4EHxrOyEfeImrndKemPRLHDLpSkuHUP9BmKn59TGly2Jk',
+        mint: [
+            {tokenAddress: tokenA, amount: amountADesired},
+            {tokenAddress: tokenB, amount: amountBDesired},
+        ],
+        unlock: [],
+    };
+
+    const receipt = await sendSimpleMessage(message);
+
+    //await printBalances('\nBalances after operation');
+
+    if (showEvents) {
+        printEvents(receipt, crossChainLayerContract);
+    }
+}
+
+
+main(true);
---
+++ scripts/CurveLite/utils.js
@@ -0,0 +1,56 @@
+const {
+    useContract,
+    getContract,
+    loadContractAddress,
+    printContractBalance,
+} = require('../utils.js');
+
+//const factoryArtifact = require("@uniswap/v2-core/build/UniswapV2Factory.json");
+//const routerArtifact = require("@uniswap/v2-periphery/build/IUniswapV2Router02");
+
+
+async function printBalances(name) {
+    const tokenTKAAddress = loadContractAddress('TKA');
+    const tokenTKBAddress = loadContractAddress('TKB');
+    const tokenWETHAddress = loadContractAddress('WETH');
+    //const factoryContract = await getContract('UniswapV2Factory', 'UniswapV2Factory', factoryArtifact,  process.env.UNISWAPV2_FACTORY_ADDRESS);
+    //const tokenLPABAddress = await factoryContract.getPair(tokenTKAAddress, tokenTKBAddress)
+
+    const tokenAddresses = {
+        'TKA': tokenTKAAddress,
+        'TKB': tokenTKBAddress,
+        'WETH': tokenWETHAddress,
+        //'LP-TKA-TKB': tokenLPABAddress,
+    };
+
+    // console.log(`----------------- ${name}:`);
+
+    // const cclContract = await useContract('ICrossChainLayer', process.env.EVM_CCL_ADDRESS);
+    // await printContractBalance(
+    //     'CrossChainLayer', await cclContract.getAddress(), tokenAddresses,
+    //     ['TKA', 'TKB', 'LP-TKA-TKB']
+    // )
+
+    // const appProxyContract = await getContract('CurveLiteProxy', 'CurveLiteProxy', null, process.env.UNISWAPV2_PROXY_ADDRESS);
+    // await printContractBalance(
+    //     'CurveLiteProxy', await appProxyContract.getAddress(), tokenAddresses,
+    //     ['TKA', 'TKB', 'LP-TKA-TKB']
+    // )
+
+    // const appContract = await getContract('UniswapV2Router02', 'UniswapV2Router02', routerArtifact, process.env.UNISWAPV2_ROUTER02_ADDRESS);
+    // await printContractBalance(
+    //     'UniswapV2Router02', await appContract.getAddress(), tokenAddresses,
+    //     ['TKA', 'TKB', 'LP-TKA-TKB']
+    // )
+
+    // const pairContractAddress = tokenLPABAddress;
+    // await printContractBalance(
+    //     'TKA-TKB Pair', pairContractAddress, tokenAddresses,
+    //     ['TKA', 'TKB', 'LP-TKA-TKB']
+    //  )
+}
+
+
+module.exports = {
+    printBalances,
+};
