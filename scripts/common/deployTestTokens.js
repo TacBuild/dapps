09a8df0a4794bc4c006a3568458b1ed9b4880d66
sergey-msu <spmathf@gmail.com>
2024-10-09 14:52:47 +0300

    fixes

--- scripts/common/deployTestTokens.js
+++ scripts/common/deployTestTokens.js
@@ -4,10 +4,10 @@ const { deployToken } = require('../utils.js');
 async function main(suffix) {
     const tokenAAddress = await deployToken(
         `stTON${suffix}`, `stTON${suffix}`, 9, 
-        'EQDJfaGp5pgN8oVGyCQI0AvUPMiuMyzaWq7Ckdf_wVZYm1IY')
+        'EQBFqkxrlEW5hZ_oEW0sBFWJjQVYsfy8CQBkVS53DsVtvB8U')
     const tokenBAddress = await deployToken(
         `TAC'${suffix}`, `TAC${suffix}`, 9, 
-        'EQC7-W1nM4DwUnI4_vGnQcLjgwYSw6hQixCdBF1XD_rmZAYZ')
+        'EQCJZiRkO77bNT8vDOsqCev5Rx4urCRJ6pcZpQb8go0I_T06')
 
     console.log(tokenAAddress, tokenBAddress);    
 }
--- scripts/common/deployWETHToken.js
+++ scripts/common/deployWETHToken.js
@@ -2,7 +2,10 @@ const { deployToken } = require('../utils.js');
 
 
 async function main() {
-    const wethAddress = await deployToken('Token wETH', 'wETH', 9, 'EQD_OypVyiEgi7PqXNHTHuGx7ANKj8ZLX_DzSfVxV_oTt9aw');
+    const wethAddress = await deployToken(
+        'Token wETH', 'wETH', 9, 
+        'EQD_OypVyiEgi7PqXNHTHuGx7ANKj8ZLX_DzSfVxV_oTt9aw'
+    );
 
     console.log(wethAddress);  
 }
