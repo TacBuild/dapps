aad0b8920960cae1cc9a2ae50fa4a61391be9940
Andrey Vikhlyantsev <av@tonappchain.fi>
2024-11-15 13:45:18 +0000

    L2-71 add btns to tilt

--- tilt/deploy.sh
+++ tilt/deploy.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
-# Function to parse addresses.json and update .env file
+# Function to update .env, .env.tilt.dapps file from parsed addresses.json. If a var exists, it is updated, created otherwise
 update_env_var() {
     VAR_NAME=$1
     VAR_VALUE=$2
+
+    # .env
     if grep -q "^$VAR_NAME=" .env; then
         sed -i "s/^$VAR_NAME=.*/$VAR_NAME=$VAR_VALUE/" .env
     else
         echo "$VAR_NAME=$VAR_VALUE" >> .env
+    fi
+
+    # .env.tilt.dapps
+    if grep -q "^$VAR_NAME=" .env.tilt.dapps; then
+        sed -i "s/^$VAR_NAME=.*/$VAR_NAME=$VAR_VALUE/" .env.tilt.dapps
+    else
+        echo "$VAR_NAME=$VAR_VALUE" >> .env.tilt.dapps
     fi
 }
 
@@ -36,6 +45,9 @@ append_uniswap_to_env() {
     VAR3=$(echo $ADDRESSES | jq -r .UniswapV2Proxy)
     update_env_var "UNISWAPV2_PROXY_ADDRESS" "$VAR3"
 }
+
+# .env.tilt.dapps is later needed for sequencer as manual.json
+touch .env.tilt.dapps
 
 npm run deploy-weth-token
 npm run deploy-stton
