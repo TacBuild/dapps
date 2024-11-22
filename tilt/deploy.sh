e039ff692199cc3aca0ff54e5f44bbdf75dc80f2
anvi <avikhlyantsev@fix.ru>
2024-11-22 16:26:12 +0300

    L2-93 updates for tilt

--- tilt/deploy.sh
+++ tilt/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export $(cat .env | xargs)
+export $(grep -v '^#' .env | xargs)
 
 NETWORK=""
 
@@ -15,22 +15,12 @@ elif [[ $DEPLOY_ENV == "mainnet" ]]; then
     NETWORK="tac_mainnet"
 fi
 
-echo "Deploying UniswapV2Proxy to $NETWORK"
-
-# Function to update .env, .env.tilt.dapps file from parsed addresses.json. If a var exists, it is updated, created otherwise
-
-# .env.tilt.dapps is later needed for sequencer as manual.json
-# touch .env.tilt.dapps
-
 npx hardhat --network $NETWORK run ./scripts/common/deployStTON.ts
 npx hardhat --network $NETWORK run ./scripts/common/deployTAC.ts
-
 npx hardhat --network $NETWORK run ./scripts/UniswapV2/deploy.ts
-
 npx hardhat --network $NETWORK run ./scripts/UniswapV2/addLiquidity.ts
 
-# Create a signal file to indicate the deploy script is done
-touch /tmp/deploy_done
+echo "------------------DEPLOY FINISHED------------------"
 
 # Keep container running
 sleep infinity
