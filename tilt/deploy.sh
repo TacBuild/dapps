da8f8eb3ba0ae1b549abfbebdc24df559f8e82ca
anvi <avikhlyantsev@fix.ru>
2024-12-18 17:20:42 +0300

    L2-157  dapps deploy tilt success

--- tilt/deploy.sh
+++ tilt/deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+# you can use this script to deploy dapps 1) on tacchain from your local machine 2) on local node with tilt
+# for case 1) populate your .env before running, for case 2) next command won't make anything
 export $(cat .env | xargs)
 
 NETWORK=""
 
-if [[ -z $DEPLOY_ENV ]]; then
+if [ -z "$DEPLOY_ENV" ]; then
     echo "DEPLOY_ENV undefined in env"
     exit 1
-elif [[ $DEPLOY_ENV == "localhost" ]]; then
+elif [ "$DEPLOY_ENV" == "localhost" ]; then
     NETWORK="localhost"
-elif [[ $DEPLOY_ENV == "testnet" ]]; then
+elif [ "$DEPLOY_ENV" == "testnet" ]; then
         NETWORK="tac_testnet"
-elif [[ $DEPLOY_ENV == "mainnet" ]]; then
+elif [ "$DEPLOY_ENV" == "mainnet" ]; then
     NETWORK="tac_mainnet"
 fi
 
