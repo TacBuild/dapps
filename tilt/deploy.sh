daa6bca56b372aceee0214a83cd872bf7ffc1f0c
anvi <avikhlyantsev@fix.ru>
2025-01-23 11:06:37 +0300

    L2-157 rm sleep from deploy.sh

--- tilt/deploy.sh
+++ tilt/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export $(cat .env | xargs)
-
 NETWORK=""
 if [ -z "$DEPLOY_ENV" ]; then
     echo "DEPLOY_ENV undefined in env"
@@ -13,17 +11,12 @@ elif [ "$DEPLOY_ENV" = "testnet" ]; then
 elif [ "$DEPLOY_ENV" = "mainnet" ]; then
     NETWORK="tac_mainnet"
 fi
-
 npx hardhat --network $NETWORK run ./scripts/depositTac.ts
 npx hardhat --network $NETWORK run ./scripts/common/deployStTON.ts
-sleep 10
 npx hardhat --network $NETWORK run ./scripts/common/deployTAC.ts
-sleep 10
 npx hardhat --network $NETWORK run ./scripts/UniswapV2/deploy.ts
 npx hardhat --network $NETWORK run ./scripts/UniswapV2/addLiquidityWithNative.ts
-sleep 10
 npx hardhat --network $NETWORK run ./scripts/UniswapV2/swapExactTokensForETH.ts
-sleep 10
 npx hardhat --network $NETWORK run ./scripts/UniswapV2/addLiquidity.ts
 npx hardhat --network $NETWORK run ./scripts/common/deploySimpleStorage.ts
 echo "------------------DEPLOY FINISHED------------------"
