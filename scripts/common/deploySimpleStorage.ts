4ef44da58ee3292a11abbaf6be34a09cb65902ab
anvi <avikhlyantsev@fix.ru>
2025-01-28 17:02:11 +0300

    L2-157 deploy with deployer

--- scripts/common/deploySimpleStorage.ts
+++ scripts/common/deploySimpleStorage.ts
@@ -1,16 +1,18 @@
+import { BaseContract, ContractFactory } from 'ethers';
 import { ethers } from "hardhat";
 import path from 'path';
+import simpleStorageArtifact from '../../artifacts/contracts/test/SimpleStorage.sol/SimpleStorage.json';
 
 import { saveContractAddress } from "tac-l2-ccl";
 
 
 async function main() {
+    const deployer = new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY!, ethers.provider);
     const addressesFilePath = path.resolve(__dirname, '../../addresses.json');
-    const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
-    const simpleStorage = await SimpleStorage.deploy();
 
+    const simpleStorageFactory = new ContractFactory(simpleStorageArtifact.abi, simpleStorageArtifact.bytecode, deployer);
+    const simpleStorage = (await simpleStorageFactory.deploy()) as BaseContract;
     const address = await simpleStorage.getAddress();
-
     saveContractAddress(addressesFilePath, 'simpleStorage', address);
     console.log("SimpleStorage deployed to: ", address);
 }
