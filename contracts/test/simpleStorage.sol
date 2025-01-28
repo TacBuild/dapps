311de62f5ad0eece37d4d5dcd743c9eb4db05004
anvi <avikhlyantsev@fix.ru>
2025-01-28 16:10:10 +0300

    L2-157 SimpleStorage is TacProxyV1

--- contracts/test/simpleStorage.sol
+++ contracts/test/simpleStorage.sol
@@ -1,10 +1,17 @@
 pragma solidity ^0.8.0;
-contract SimpleStorage {
+
+import { TacHeaderV1 } from "tac-l2-ccl/contracts/L2/Structs.sol";
+import { TacProxyV1 } from "tac-l2-ccl/contracts/proxies/TacProxyV1.sol";
+
+contract SimpleStorage is TacProxyV1 {
     uint256 private storedData;
+
     // Function to set a value
-    function set(uint256 x) public {
-        storedData = x;
+    function set(bytes calldata tacHeader, bytes calldata arguments) public {
+        uint256 value = abi.decode(arguments, (uint256));
+        storedData = value;
     }
+
     // Function to retrieve the stored value
     function get() public view returns (uint256) {
         return storedData;
