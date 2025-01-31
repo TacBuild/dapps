5e444b67f195d0b16094e6a2d0e0bac053c3cfda
sergey-msu <spmathf@gmail.com>
2025-01-31 21:20:26 +0400

    remove via flag

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,7 +14,6 @@ const config: HardhatUserConfig = {
       {
         version: "0.8.25",
         settings: {
-          viaIR: true,
           optimizer: {
             enabled: true,
             runs: 200
