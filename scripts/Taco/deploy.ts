58d87d530456ad8537c2146bf58346940905ffa8
Aleksei Kokinos <akkokinos@gmail.com>
2025-02-01 23:06:17 +0700

    add taco config file

--- scripts/Taco/deploy.ts
+++ scripts/Taco/deploy.ts
@@ -2,33 +2,38 @@ import hre, { ethers } from 'hardhat';
 import path from 'path';
 import { TacoProxy } from '../../typechain-types/';
 import { deploy, loadTacContracts, saveContractAddress } from "tac-l2-ccl";
+import { Signer } from 'ethers';
+import { tacoTestnetConfig } from './config/testnetConfig';
+import { tacoMainnetConfig } from './config/mainnetConfig';
 
+export async function deployTacoProxy(deployer: Signer, tacoConfig: TacoConfig, crossChainLayerAddress: string): Promise<TacoProxy> {
+    // Proxy
+    const tacoProxy = await deploy<TacoProxy>(
+        deployer,
+        hre.artifacts.readArtifactSync('TacoProxy'),
+        [tacoConfig.tacoV2Proxy02, tacoConfig.tacoFeeRouteProxy, crossChainLayerAddress],
+        undefined,
+        true
+    );
+
+    return tacoProxy;
+}
 
 async function main() {
     const deployer = new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY!, ethers.provider);
     const addressesFilePath = path.resolve(__dirname, '../../addresses.json');
     const tacContracts = await loadTacContracts(addressesFilePath, deployer);
 
-    // Taco protocol is already deployed, save addresses
-    const tacoV2Proxy02Address = process.env.DODO_V2PROXY02_ADDRESS as string;
-    const tacoFeeRouteProxyAddress = process.env.DODO_FEEROUTEPROXY_ADDRESS as string
-    const tacoDFMFactoryAddress = process.env.DODO_DVMFACTORY_ADDRESS as string
-    const tacoApproveAddress = process.env.DODO_APPROVE_ADDRESS as string
-    const tacoWETHAddress = process.env.DODO_WETH_ADDRESS as string
-    saveContractAddress(addressesFilePath, 'tacoV2Proxy02', tacoV2Proxy02Address);
-    saveContractAddress(addressesFilePath, 'tacoFeeRouteProxy', tacoFeeRouteProxyAddress);
-    saveContractAddress(addressesFilePath, 'tacoDFMFactory', tacoDFMFactoryAddress);
-    saveContractAddress(addressesFilePath, 'tacoApprove', tacoApproveAddress);
-    saveContractAddress(addressesFilePath, 'tacoWETH', tacoWETHAddress);
+    const config = process.env.DEPLOY_ENV === "mainnet" ? tacoMainnetConfig : tacoTestnetConfig;
 
-    // Proxy
-    const tacoProxy = await deploy<TacoProxy>(
-        deployer,
-        hre.artifacts.readArtifactSync('TacoProxy'),
-        [tacoV2Proxy02Address, tacoFeeRouteProxyAddress, await tacContracts.crossChainLayer.getAddress()],
-        undefined,
-        true
-    );
+    // Taco protocol is already deployed, save addresses
+    saveContractAddress(addressesFilePath, 'tacoV2Proxy02', config.tacoV2Proxy02);
+    saveContractAddress(addressesFilePath, 'tacoFeeRouteProxy', config.tacoFeeRouteProxy);
+    saveContractAddress(addressesFilePath, 'tacoDVMFactory', config.tacoDVMFactory);
+    saveContractAddress(addressesFilePath, 'tacoApprove', config.tacoApprove);
+    saveContractAddress(addressesFilePath, 'tacoWETH', config.tacoWETH);
+
+    const tacoProxy = await deployTacoProxy(deployer, config, await tacContracts.crossChainLayer.getAddress());
 
     saveContractAddress(addressesFilePath, 'tacoProxy', await tacoProxy.getAddress());
 }
