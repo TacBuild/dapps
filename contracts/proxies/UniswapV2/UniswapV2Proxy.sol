fedbb998a8f30da288ecb6a134524ab964cba2d2
Aleksei Kokinos <akkokinos@gmail.com>
2025-02-17 17:27:26 +0700

    update solc version

--- contracts/proxies/UniswapV2/UniswapV2Proxy.sol
+++ contracts/proxies/UniswapV2/UniswapV2Proxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.28;
 
 import { IUniswapV2Router02 } from '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 import { TransferHelper } from '@uniswap/lib/contracts/libraries/TransferHelper.sol';
