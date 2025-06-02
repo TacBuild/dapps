8ef46ac01dca64f98a6fa11c018c5504fa5f0386
ShiftPhase <a.kovalenko@entangle.fi>
2025-06-02 20:22:39 +0700

    prettify

--- scripts/TacVault/TacVaultDeploy.ts
+++ scripts/TacVault/TacVaultDeploy.ts
@@ -4,8 +4,6 @@ import { deployUpgradable } from '@tonappchain/evm-ccl'
 import { DeployProxyOptions } from "@openzeppelin/hardhat-upgrades/dist/utils";
 import hre from 'hardhat';
 import { tacVaultTestnetConfig } from "./config/TacVaultTestnetConfig";
-import { deployTacSmartAccount } from "../TacSmartAccountFactory/SABlueprintDeploy";
-import { deployTacSAFactory } from "../TacSmartAccountFactory/FactoryDeploy";
 
 const proxyOptsUUPS: DeployProxyOptions = {
     kind: "uups"
@@ -30,16 +28,3 @@ export async function deployTacVault(
     await tacVaultProxy.waitForDeployment();
     return tacVaultProxy;
 }
-
-async function main() {
-    const provider = hre.ethers.provider;
-    const wallet = new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY || "", provider);
-    // console.log(await wallet.getAddress());
-    // const tacSA = await deployTacSmartAccount(wallet);
-    // console.log(await tacSA.getAddress());
-    // const tacSAFactory = await deployTacSAFactory(wallet, "0xeAB80f5369689a2D142f25E654d9822A7725028B");
-    const tacVaultProxy = await deployTacVault(wallet, "0x4f3b05a601B7103CF8Fc0aBB56d042e04f222ceE", "0x510ee99eD721107851D692f761198E3dE4e9310D");
-    console.log("TacVaultProxy deployed to:", await tacVaultProxy.getAddress());
-}
-
-main();
