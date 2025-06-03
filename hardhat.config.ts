b4d4ee67dfd4f3ae13d7b0a4793bc71c586d87f4
ShiftPhase <a.kovalenko@entangle.fi>
2025-06-03 19:20:40 +0700

    block height update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -35,7 +35,7 @@ const config: HardhatUserConfig = {
       allowBlocksWithSameTimestamp: true,
       forking: {
         url: TAC_TESTNET_SPB_URL,
-        blockNumber: 1031025,
+        blockNumber: 1256737,
       },
     },
     localhost: {
