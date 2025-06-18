1f83327cee81c5610e7dfb5baf0211143d33287a
ShiftPhase <a.kovalenko@entangle.fi>
2025-06-18 18:07:52 +0700

    upgrade Tac sa factory on turin

--- contracts/TacSmartAccounts/TacSmartAccount.sol
+++ contracts/TacSmartAccounts/TacSmartAccount.sol
@@ -33,6 +33,12 @@ contract TacSmartAccount is Initializable {
         emit Executed(target, value, data);
     }
 
+    function delegatecall(address target, bytes calldata data) external onlyOwner returns(bool success, bytes memory returnData) {
+        (success, returnData) = target.delegatecall(data);
+        require(success, "Delegatecall failed");
+        emit Executed(target, 0, data);
+    }
+
     function approve(address token, address to, uint256 amount) external onlyOwner{
         IERC20(token).approve(to, amount);
     }
--- scripts/TacSmartAccountFactory/config/testnetConfig.ts
+++ scripts/TacSmartAccountFactory/config/testnetConfig.ts
@@ -5,5 +5,5 @@ export interface TacSAFactoryTestnetConfig {
 
 export const tacSAFactoryDeployments: TacSAFactoryTestnetConfig = {
     proxyAddress: "0x95e23BBa93b6c9Ef89A1bFB2659B020e9382C060",
-    blueprintAddress: "0x16AB90C97abC36abA9C358aB6DD564f468B9b05F"
+    blueprintAddress: "0xA62E11572CBeED7C24D6Ea0758725b12736822CB"
 };
