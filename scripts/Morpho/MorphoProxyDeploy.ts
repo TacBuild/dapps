c741512beecfddbe035211e4ce4a636a322e9ae2
ShiftPhase <a.kovalenko@entangle.fi>
2025-07-01 22:14:41 +0700

    morpho deploy

--- scripts/Morpho/MorphoProxyDeploy.ts
+++ scripts/Morpho/MorphoProxyDeploy.ts
@@ -4,6 +4,7 @@ import { deployUpgradable } from '@tonappchain/evm-ccl'
 import { DeployProxyOptions } from "@openzeppelin/hardhat-upgrades/dist/utils";
 import hre from 'hardhat';
 import { morphoProxyDeployments, morphoTestnetConfig } from "./config/testnetConfig";
+import { morphoMainnetConfig, morphoMainnetProxyDeployments } from "./config/mainnetConfig";
 
 const proxyOptsUUPS: DeployProxyOptions = {
     kind: "uups",
@@ -30,7 +31,27 @@ export async function deployMorphoProxy(
     return morphoProxy;
 }
 
+export async function deployMorphoProxyMainnet(
+    deployer: Signer,
+    crossChainLayerAddress: string,
+    tacSAFactoryAddress: string
+): Promise<MorphoProxy> {
+    const morphoProxy = await deployUpgradable<MorphoProxy>(
+        deployer,
+        hre.artifacts.readArtifactSync('MorphoProxy'),
+        [crossChainLayerAddress, morphoMainnetConfig.morphoAddress, morphoMainnetConfig.urdAddress, morphoMainnetConfig.metaMorphoV1_1Address, tacSAFactoryAddress],
+        proxyOptsUUPS,
+        undefined,
+        true
+    );
+
+    await morphoProxy.waitForDeployment();
+    return morphoProxy;
+}
+
 async function main() {
     const [deployer] = await hre.ethers.getSigners();
-    deployMorphoProxy(deployer, morphoProxyDeployments.crossChainLayerAddress, morphoProxyDeployments.smartAccountFactoryAddress);
+    deployMorphoProxyMainnet(deployer, morphoMainnetProxyDeployments.crossChainLayerAddress, morphoMainnetProxyDeployments.smartAccountFactoryAddress);
 }
+
+// main();
--- scripts/TacSmartAccountFactory/config/mainnetConfig.ts
+++ scripts/TacSmartAccountFactory/config/mainnetConfig.ts
@@ -4,6 +4,6 @@ export interface TacSAFactoryMainnetConfig {
 }
 
 export const tacSAFactoryDeployments: TacSAFactoryMainnetConfig = {
-    proxyAddress: "0xa0bC98B4C531bD4Be025Ee4809B74696f836e0Ad",
+    proxyAddress: "0x070820Ed658860f77138d71f74EfbE173775895b",
     blueprintAddress: "0xeAB80f5369689a2D142f25E654d9822A7725028B"
 };
