2d9012e6a393e862f867af3f41cb07a65122f458
ShiftPhase <a.kovalenko@entangle.fi>
2025-07-02 16:47:54 +0700

    S3 (factory)

--- contracts/TacSmartAccounts/TacSAFactory.sol
+++ contracts/TacSmartAccounts/TacSAFactory.sol
@@ -3,11 +3,11 @@ pragma solidity 0.8.28;
 
 import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {TacSmartAccount} from "./TacSmartAccount.sol";
 
-contract TacSAFactory is OwnableUpgradeable, UUPSUpgradeable {
+contract TacSAFactory is Ownable2StepUpgradeable, UUPSUpgradeable {
     UpgradeableBeacon public beacon;
     mapping(address application => mapping(bytes32 id => address smartAccount)) public smartAccounts;
 
@@ -16,6 +16,7 @@ contract TacSAFactory is OwnableUpgradeable, UUPSUpgradeable {
     function initialize(
         address _initBlueprint
     ) external initializer {
+        __Ownable2Step_init();
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
         beacon = new UpgradeableBeacon(_initBlueprint, address(this));
