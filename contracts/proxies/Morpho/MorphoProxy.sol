7c865d70b97a0ff1bf5d8ce249cbbe49e5b12f68
ShiftPhase <a.kovalenko@entangle.fi>
2025-07-03 21:24:54 +0700

    morpho upgraded due to TMA request

--- contracts/proxies/Morpho/MorphoProxy.sol
+++ contracts/proxies/Morpho/MorphoProxy.sol
@@ -311,9 +311,14 @@ contract MorphoProxy is
         bytes memory returnData = ITacSmartAccount(user).execute(
             args.vault,
             0,
-            abi.encodeWithSelector(IMorphoVault.deposit.selector, args.assets, address(this))
+            abi.encodeWithSelector(IMorphoVault.deposit.selector, args.assets, user)
         );
         uint256 shares = abi.decode(returnData, (uint256));
+        ITacSmartAccount(user).execute(
+            args.vault,
+            0,
+            abi.encodeWithSelector(IERC20.transfer.selector, address(this), IERC20(args.vault).balanceOf(user))
+        );
         require(args.assets.rDivUp(shares) <= args.maxSharePriceE27, "Slippage");
         emit Deposit(args.vault, args.assets);
         TokenAmount[] memory tokensToBridge = new TokenAmount[](1);
--- scripts/Morpho/MorphoProxyUpgrade.ts
+++ scripts/Morpho/MorphoProxyUpgrade.ts
@@ -2,7 +2,7 @@ import { Signer } from "ethers";
 import { MorphoProxy } from "../../typechain-types";
 import { DeployProxyOptions } from "@openzeppelin/hardhat-upgrades/dist/utils";
 import hre from 'hardhat';
-import { morphoProxyDeployments } from "./config/testnetConfig";
+import { morphoMainnetProxyDeployments } from "./config/mainnetConfig";
 
 const proxyOptsUUPS: DeployProxyOptions = {
     kind: "uups",
@@ -13,7 +13,7 @@ export async function upgradeMorphoProxy(
 ): Promise<MorphoProxy> {
     const [signer] = await hre.ethers.getSigners();
     const factory = await hre.ethers.getContractFactory("MorphoProxy", signer);
-    const morphoProxy = await hre.upgrades.upgradeProxy(morphoProxyDeployments.proxyAddress, factory, proxyOptsUUPS);
+    const morphoProxy = await hre.upgrades.upgradeProxy(morphoMainnetProxyDeployments.proxyAddress, factory, proxyOptsUUPS);
     await morphoProxy.waitForDeployment();
     return morphoProxy;
 } 
