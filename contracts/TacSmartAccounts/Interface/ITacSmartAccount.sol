7674ee8ef58dd4fc085e5712690454bf0bee04e4
ShiftPhase <a.kovalenko@entangle.fi>
2025-07-08 18:26:06 +0700

    update smart account interface

--- contracts/TacSmartAccounts/Interface/ITacSmartAccount.sol
+++ contracts/TacSmartAccounts/Interface/ITacSmartAccount.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.28;
 import {TokenAmount, NFTAmount} from "@tonappchain/evm-ccl/contracts/core/Structs.sol";
 interface ITacSmartAccount {
     function execute(address target, uint256 value, bytes calldata data) external payable returns(bytes memory);
-    function bridgeTokens(bytes calldata tacHeader, TokenAmount[] memory tokens, NFTAmount[] memory nfts, string memory payload, address crossChainLayer) external;
+    function executeUnsafe(address target, uint256 value, bytes calldata data) external payable returns(bool success, bytes memory returnData);
+    function delegatecall(address target, bytes calldata data) external returns(bool success, bytes memory returnData);
+    function createOneTimeTicket(address caller) external;
+    function revokeOneTimeTicket(address caller) external;
+    function multicall(address[] calldata targets, uint256[] calldata values, bytes[] calldata data) external returns(bytes[] memory);
     function approve(address token, address to, uint256 amount) external;
 }
