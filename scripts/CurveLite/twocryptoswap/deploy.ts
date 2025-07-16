3b92eb237ab359e93050d57cdedd16cf091115ed
Dmitrii <dg@tonappchain.fi>
2025-07-16 18:11:25 +0300

    deploy back

--- scripts/CurveLite/twocryptoswap/deploy.ts
+++ scripts/CurveLite/twocryptoswap/deploy.ts
@@ -1,4 +1,4 @@
-import { ethers } from 'hardhat';
+import { ethers, upgrades  } from 'hardhat';
 import { loadTacContracts, saveContractAddress } from "@tonappchain/evm-ccl";
 import { deployCurveLiteTwocryptoswapProxy } from './deployProxy';
 import { deployTacSAFactory } from '../../TacSmartAccountFactory/FactoryDeploy';
@@ -12,9 +12,16 @@ async function main() {
 
     const tacContracts = await loadTacContracts(addressesFilePath, deployer);
 
-    const CurveLiteTwocryptoswapProxy = await deployCurveLiteTwocryptoswapProxy(deployer, await  tacContracts.crossChainLayer.getAddress());
+    // const tacSmartAccount = await deployTacSmartAccount(deployer);
+
+    // const tacSAFactory = await deployTacSAFactory(deployer, await tacSmartAccount.getAddress());
+
+    const CurveLiteTwocryptoswapProxy = await deployCurveLiteTwocryptoswapProxy(deployer, "0x070820Ed658860f77138d71f74EfbE173775895b", await tacContracts.crossChainLayer.getAddress());
 
     saveContractAddress(addressesFilePath, 'CurveLiteTwocryptoswapProxy', await CurveLiteTwocryptoswapProxy.getAddress());
+
+
+
 }
 
 
