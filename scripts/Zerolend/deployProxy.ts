2e85078a02a5f0bb2038c02ff17d2bde516b5d0e
Dmitrii <dg@tonappchain.fi>
2025-08-07 13:22:49 +0300

    deploy fix

--- scripts/Zerolend/deployProxy.ts
+++ scripts/Zerolend/deployProxy.ts
@@ -2,10 +2,16 @@ import hre from 'hardhat';
 import { ZerolendPoolProxy } from '../../typechain-types';
 import { Signer } from 'ethers';
 import { deployUpgradable } from '@tonappchain/evm-ccl'
-import { proxyOptsUUPS } from "../utils"
+import { DeployProxyOptions } from "@openzeppelin/hardhat-upgrades/dist/utils";
 
 export async function deployZerolendPoolProxy(deployer: Signer, appAddress: string, tacSAFactoryAddress: string, crossChainLayerAddress: string): Promise<ZerolendPoolProxy> {
     // Proxy
+    
+    const proxyOptsUUPS: DeployProxyOptions = {
+        kind: "uups",
+        unsafeAllow: ["constructor"]
+    };
+    
     const zerolendPoolProxy = await deployUpgradable<ZerolendPoolProxy>(
         deployer,
         hre.artifacts.readArtifactSync('ZerolendPoolProxy'),
