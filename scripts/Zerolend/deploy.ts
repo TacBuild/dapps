3964340758f0ed5f725468d3868721cc98f6f506
ShiftPhase <a.kovalenko@entangle.fi>
2025-09-19 18:36:46 +0700

    Zeroland deployed on mainnet

--- scripts/Zerolend/deploy.ts
+++ scripts/Zerolend/deploy.ts
@@ -1,30 +1,13 @@
 import { ethers } from 'hardhat';
-import { loadTacContracts, saveContractAddress } from "@tonappchain/evm-ccl";
 import { deployZerolendPoolProxy } from './deployProxy';
-import { deployTacSAFactory } from '../TacSmartAccountFactory/FactoryDeploy';
-import { deployTacSmartAccount } from '../TacSmartAccountFactory/SABlueprintDeploy';
 
-import { zerolendPoolConfig } from "./config/ZerolendConfig";
-import path from 'path';
+import { zerolandPoolConfigMainnet } from "./config/ZerolendConfig";
+import { mainnetConfig } from "../config/mainnetConfig";
 
 async function main() {
-    const deployer = new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY!, ethers.provider);
-
-    const addressesFilePath = path.resolve(__dirname, '../../addresses.json');
-
-    const tacContracts = await loadTacContracts(addressesFilePath, deployer);
-
-    const tacSmartAccount = await deployTacSmartAccount(deployer);
-
-    const tacSAFactory = await deployTacSAFactory(deployer, await tacSmartAccount.getAddress());
-
-    const zerolendPoolProxy = await deployZerolendPoolProxy(deployer, zerolendPoolConfig.appAddress, await tacSAFactory.getAddress(), await  tacContracts.crossChainLayer.getAddress());
-
-
-    saveContractAddress(addressesFilePath, 'ZerolendSA', await tacSAFactory.getAddress());
-    saveContractAddress(addressesFilePath, 'ZerolendPoolProxy', await zerolendPoolProxy.getAddress());
-
+    const [deployer] = await ethers.getSigners();
+    const zerolendPoolProxy = await deployZerolendPoolProxy(deployer, zerolandPoolConfigMainnet.appAddress, mainnetConfig.tacSAFactoryAddress, mainnetConfig.crosschainLayerAddress);
+    console.log(await zerolendPoolProxy.getAddress());
 }
-
 
 main();
