5c26c4fb40bfac55271ad7e15ea45c983258bfd3
Vladislav Kokosh <v.kokosh@pixelplex.io>
2025-09-23 15:08:04 +0300

    Added Tac address in config

--- scripts/LiquidStakingProxy/config/mainnetConfig.ts
+++ scripts/LiquidStakingProxy/config/mainnetConfig.ts
@@ -8,6 +8,6 @@ export interface LiquidStakingConfig {
 export const liquidStakingConfig: LiquidStakingConfig = {
   crossChainLayer: "0x9fee01e948353E0897968A3ea955815aaA49f58d",
   smartAccountFactory: "0x070820Ed658860f77138d71f74EfbE173775895b",
-  liquidTacToken: null,
+  liquidTacToken: "0xCff690a580d591432e46d37C6221DC2d9BCE4f4a",
   owner: null,
 };
