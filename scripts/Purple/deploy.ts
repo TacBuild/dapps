a5efcbd3d7a82476db9b3b07805f2e9cfc42bf09
anvi <av@tonappchain.fi>
2025-12-18 17:07:19 +0300

    L2-547 deploy Purple to tilt ok

--- scripts/Purple/deploy.ts
+++ scripts/Purple/deploy.ts
@@ -1,30 +1,35 @@
-import { deploy } from "@tonappchain/evm-ccl";
-import { PurpleDapp, LpFactory, LpToken } from "../../typechain-types";
-import hre from "hardhat";
+import path from "path";
 import { Signer } from "ethers";
+import hre, {ethers} from "hardhat";
+
+import { deploy, saveContractAddress } from "@tonappchain/evm-ccl";
+
+import { PurpleDapp, LpFactory, LpToken } from "../../typechain-types";
 
 export async function deployPurpleDapp(
     deployer: Signer,
 ): Promise<{ purpleDapp: PurpleDapp, purpleFactory: LpFactory }> {
-    
     const lpBlueprint = await deploy<LpToken>(deployer, hre.artifacts.readArtifactSync("LpToken"), []);
     const purpleDapp = await deploy<PurpleDapp>(deployer, hre.artifacts.readArtifactSync("PurpleDapp"), []);
     await purpleDapp.waitForDeployment();
     const purpleFactory = await deploy<LpFactory>(deployer, hre.artifacts.readArtifactSync("LpFactory"), [await purpleDapp.getAddress(), await lpBlueprint.getAddress()]);
     await purpleFactory.waitForDeployment();
     await purpleDapp.setLpFactory(await purpleFactory.getAddress());
-    console.log("PurpleDapp deployed to:", purpleDapp.target);
     return { purpleDapp, purpleFactory };
 }
 
 async function main() {
-    const [deployer] = await hre.ethers.getSigners();
+    const deployer = new ethers.Wallet(process.env.DEPLOYER_PRIVATE_KEY!, ethers.provider);
     const { purpleDapp, purpleFactory } = await deployPurpleDapp(deployer);
-    console.log("PurpleDapp deployed to:", await purpleDapp.getAddress());
-    console.log("PurpleFactory deployed to:", await purpleFactory.getAddress());
+    console.log("PurpleDapp deployed at:", await purpleDapp.getAddress());
+    console.log("PurpleFactory deployed at:", await purpleFactory.getAddress());
+
+    const addressesFilePath = path.resolve(__dirname, '../../addresses.json');
+    saveContractAddress(addressesFilePath, 'PurpleDapp', await purpleDapp.getAddress());
+    saveContractAddress(addressesFilePath, 'PurpleFactory', await purpleFactory.getAddress());
 }
 
-// main().catch((error) => {
-//     console.error(error);
-//     process.exitCode = 1;
-// });
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
