0ab65f8df144958dfd2faf293eb53635a3a448c5
ShiftPhase <a.kovalenko@entangle.fi>
2026-01-23 22:39:49 +0700

    deploy script cleared

--- scripts/Midas/deployMidasProxy.ts
+++ scripts/Midas/deployMidasProxy.ts
@@ -37,14 +37,3 @@ export async function deployMidasProxyUSDT(deployer: Signer, tacSAFactoryAddress
     await midasProxy.waitForDeployment();
     return midasProxy;
 }
-
-async function main() {
-    const [deployer] = await ethers.getSigners();
-    const midasProxy = await deployMidasProxyUSDT(deployer, "0x070820Ed658860f77138d71f74EfbE173775895b", "0xbD2CE9D5F2c682FCA3ce587Bf1C041ad8DDd2a69", "0x911f9aF9138284A49b29F9894571Fb86e29D1d79", "0x9fee01e948353E0897968A3ea955815aaA49f58d");
-    console.log("MidasProxyUSDT deployed to:", midasProxy.target);
-}
-
-main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-})
