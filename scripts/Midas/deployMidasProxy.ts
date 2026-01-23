78935b0f412b73ec8a458b7f8d1dc0c43d7445d4
ShiftPhase <a.kovalenko@entangle.fi>
2026-01-23 22:39:03 +0700

    DEV-58

--- scripts/Midas/deployMidasProxy.ts
+++ scripts/Midas/deployMidasProxy.ts
@@ -1,9 +1,8 @@
 import hre from 'hardhat';
-import { MidasProxy } from '../../typechain-types/';
+import { MidasProxy, MidasProxyUSDT } from '../../typechain-types/';
 import { Signer } from 'ethers';
 import { deployUpgradable } from '@tonappchain/evm-ccl'
 import { DeployProxyOptions } from "@openzeppelin/hardhat-upgrades/dist/utils";
-import { midasTestnetConfig } from "./config/testnetConfig";
 
 const proxyOptsUUPS: DeployProxyOptions = {
     kind: "uups",
@@ -24,3 +23,28 @@ export async function deployMidasProxy(deployer: Signer, tacSAFactoryAddress: st
     await midasProxy.waitForDeployment();
     return midasProxy;
 }
+
+export async function deployMidasProxyUSDT(deployer: Signer, tacSAFactoryAddress: string, depositVaultAddress: string, redemptionVaultAddress: string,  crossChainLayerAddress: string): Promise<MidasProxyUSDT> {
+    // Proxy
+    const midasProxy = await deployUpgradable<MidasProxyUSDT>(
+        deployer,
+        hre.artifacts.readArtifactSync('MidasProxyUSDT'),
+        [await deployer.getAddress(), tacSAFactoryAddress, depositVaultAddress, redemptionVaultAddress, crossChainLayerAddress],
+        proxyOptsUUPS,
+        undefined,
+        true
+    );
+    await midasProxy.waitForDeployment();
+    return midasProxy;
+}
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    const midasProxy = await deployMidasProxyUSDT(deployer, "0x070820Ed658860f77138d71f74EfbE173775895b", "0xbD2CE9D5F2c682FCA3ce587Bf1C041ad8DDd2a69", "0x911f9aF9138284A49b29F9894571Fb86e29D1d79", "0x9fee01e948353E0897968A3ea955815aaA49f58d");
+    console.log("MidasProxyUSDT deployed to:", midasProxy.target);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+})
